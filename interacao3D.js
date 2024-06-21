49a7c6001286d140de397d3e556e21f30e1bf781
hugo-nunes02 <hugo.nunes06012009@gmail.com>
2024-06-20 21:20:12 -0300

    atualização

---
+++ interacao3D.js
@@ -0,0 +1,6 @@
+let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
+window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
+var outroElemento = document.querySelector(".main_title"); 
+outroElemento.textContent = (`Olá, ${saudacao}! Bem-vindo a Animatec`);
+Element.appendChild(text)
+
